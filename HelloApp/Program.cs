fd7d4b1458c4268cf23d309b6340568ebdcf5317
RamalingappaBS <98861245+RamalingappaBS@users.noreply.github.com>
2022-11-12 16:50:45 +0530

    Update Program.cs

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Welocme to Git repositary");
+Console.WriteLine("Welcome to Git repositary");
+console.Writeline("Git and Git hub are same");
