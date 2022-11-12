d716b80e306474a0d6da147290b29edfa356b095
RamalingappaBS <bsramalingappa@gmail.com>
2022-11-12 16:52:45 +0530

    Second commit in Git

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to Git repositary");
-console.Writeline("Git and Git hub are same");
+Console.Writeline("Git and Git hub are same");
